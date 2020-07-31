17e60bad866d59f7ca4256142e65afdcdfe07109
Vlad Romaniuk <v.romaniuk@orderry.com>
2020-07-31 17:47:13 +0300

    Create UserProfile model and UserProfileManager

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError('Please specify email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(PermissionsMixin, AbstractBaseUser):
+    """
+    User model in the system
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    @property
+    def full_name(self):
+        return self.name
+
+    @property
+    def short_name(self):
+        return self.name
+
+    def __str__(self):
+        return f'{self.email}-{self.name}'
